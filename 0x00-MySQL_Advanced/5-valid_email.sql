ef891b91e6fc770ea27676efb75b94cbfe7e18a0
Brian Omondi <omondiibrian00@gmail.com>
2024-01-20 10:11:18 +0300

    Email validation

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- SQL script that creates a trigger that resets the attribute
+-- valid_email only when the email has been changed
+DELIMITER //
+CREATE TRIGGER emailValidator
+BEFORE INSERT ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> (SELECT email FROM users WHERE name = NEW.name) THEN
+        SET NEW.email = 0;
+    END IF;
+END;//
+DELIMITER ;
