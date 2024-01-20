aa936c039890f1e7aa6b01afbdadacbe84eb5e8e
Brian Omondi <omondiibrian00@gmail.com>
2024-01-20 09:26:22 +0300

    Doesn't work locally

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,8 +2,8 @@
 -- ranked by their longevity
 SELECT band_name, 
         (CASE
-            WHEN split IS NULL THEN 2022 - formed
-            ELSE split - formed
+            WHEN split IS NULL THEN (2022 - formed)
+            ELSE (split - formed)
         END) AS lifespan
 FROM metal_bands
 WHERE style = 'Glam rock'
