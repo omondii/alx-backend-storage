3220579f5c9ecbee71e6fd98911bd09156c3ccc4
Brian Omondi <omondiibrian00@gmail.com>
2024-01-25 20:36:21 +0300

    update on task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -32,14 +32,11 @@ class Cache():
         else:
             return data
         
-cache = Cache()
-
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
-
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    assert cache.get(key, fn=fn) == value
+    def get_str(self, key: str) -> Union[str, None]:
+        """ Parameterize strings passed """
+        return self.get(key, fn=lambda x: x.decode() if x is not None else None)
+    
+    def get_int(self, key: str) -> Union[str, None]:
+        """ Handles parameterazing integers """
+        return self.get(key, fn=lambda x: int(x) if x is not None else None)
+        
